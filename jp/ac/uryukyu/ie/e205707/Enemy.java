af6f3af588960798921dce1d6b9c80facff4afda
Miyagi REN <e205707@eve.u-ryukyu.ac.jp>
2020-11-23 00:36:46 +0900

    fix attak

--- jp/ac/uryukyu/ie/e205707/Enemy.java
+++ jp/ac/uryukyu/ie/e205707/Enemy.java
@@ -33,12 +33,16 @@ public class Enemy {
      * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
      * @param hero 攻撃対象
      */
-    public void attack(Hero hero){
-        if(0 < hitPoint){
-             int damage = (int)(Math.random() * attack);
+    public void attack(Hero hero){  
+        if(dead == false){
+            int damage = (int)(Math.random() * attack);
             System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
             hero.wounded(damage);
+        }else{
+            System.out.printf(name + "は倒れたため攻撃できなっかた");
         }
+        
+        
        
     }
 
